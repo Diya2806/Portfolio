771c29ef2b117e5520bbbd0a1c291e7c5cdbf4bb
Udayraj Solanki <solankius9909@gmail.com>
2026-04-28 19:44:51 +0530

    Fix TiDB SSL connection

--- db.js
+++ db.js
@@ -7,7 +7,10 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME || 'portfolio_db',
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: process.env.DB_HOST && process.env.DB_HOST !== 'localhost' ? {
+        rejectUnauthorized: false
+    } : undefined
 });
 
 module.exports = pool;
