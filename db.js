655fae4414ce4256ef27f87185b735a40a2e8845
Udayraj Solanki <solankius9909@gmail.com>
2026-04-28 19:53:10 +0530

    fix TiDB production config

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Diya123@',
-    database: process.env.DB_NAME || 'portfolio_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 4000,
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    ssl: process.env.DB_HOST && process.env.DB_HOST !== 'localhost' ? {
+
+    ssl: {
         rejectUnauthorized: false
-    } : undefined
+    }
 });
 
 module.exports = pool;
